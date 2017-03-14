49e6e98f4ae22242049fd4c933ada0c55480ae97
Rusty the Lambinator <talentedsnake@gmail.com>
2017-03-14 00:40:06 +0000

    Added WSGI interface for gunicorn

---
+++ venv/wsgi.py
@@ -0,0 +1,7 @@
+#!flask/bin/python
+from steam_prices import app
+import os
+
+#app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
+if __name__ == "__main__":
+    app.run()
