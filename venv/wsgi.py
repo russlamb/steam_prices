9a38aab2e83596d6f4aea8fd67d15dabaf6673ff
Rusty the Lambinator <talentedsnake@gmail.com>
2017-03-14 02:35:23 +0000

    added change dir to procfile

--- venv/wsgi.py
+++ venv/wsgi.py
@@ -1,7 +1,8 @@
 #!flask/bin/python
 from steam_prices import app
 import os
+    
 
-#app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
+#uwsgi -s /tmp/yourapplication.sock --manage-script-name --mount /yourapplication=myapp:app
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
